d3cbe4c59e73f24c90b6195a4a7e33bf70faebab
Ezekiel12345 <you@example.com>
2021-07-13 19:48:42 +0100

    assessment 6

--- main.kt
+++ main.kt
@@ -1,9 +1,7 @@
 fun main(args: Array<String>) {
-    Evenlyspaced(a = 2, b = 4, c = 6)
-    println()
-}
-fun Evenlyspaced(a:Int, b:Int, c:Int): Boolean {
-    return ((a + b) == 2 * c) || ((a + c) == 2 * b) || ((b + c) == 2 * a)
-    println()
-}
-
+        println(petals(7,3))
+    }
+    fun petals(timmy:Int,sarah:Int):Boolean{
+        if ((timmy %2==0) .xor (sarah %2==0)) return true
+        return false
+    }
